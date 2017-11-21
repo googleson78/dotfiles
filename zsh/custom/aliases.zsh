038306f0f0a4f5a2f7622ba467531068f4290328
Georgi Lyubenov <godzbanebane@gmail.com>
2017-11-21 11:06:27 +0200

    added sshuttle alias to home

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,3 +1,5 @@
 alias :q="exit"
 alias brightness="xrandr --output LVDS1 --brightness"
 alias -g g=" | grep"
+alias tixe=exit
+alias sshtun="sshuttle --dns -D -x grunt -r googleson78@grunt 0.0.0.0/0"
