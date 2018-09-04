db1148d13550fb3cd142f1549d71da643982e529
Georgi Lyubenov <georgi.lyubenov@chaosgroup.com>
2018-09-04 13:36:01 +0300

    Useful new alias

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -2,4 +2,5 @@ alias :q="exit"
 alias brightness="xrandr --output LVDS1 --brightness"
 alias -g g=" | grep"
 alias tixe=exit
-alias sshtun="sshuttle --dns -D -x grunt -r googleson78@grunt 0.0.0.0/0"
+alias sshtun="sshuttle --dns -D -x adjoint.fun -r googleson78@adjoint.fun 0.0.0.0/0"
+alias destroystack="find -regex '.*\.stack-work.*' -delete"
