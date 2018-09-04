f379e522d0ac29e706080090bf0dbdc1e5e0ce48
Georgi Lyubenov <georgi.lyubenov@chaosgroup.com>
2018-09-04 15:43:51 +0300

    Previous version was buggy I think?

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -3,4 +3,4 @@ alias brightness="xrandr --output LVDS1 --brightness"
 alias -g g=" | grep"
 alias tixe=exit
 alias sshtun="sshuttle --dns -D -x adjoint.fun -r googleson78@adjoint.fun 0.0.0.0/0"
-alias destroystack="find -regex '.*\.stack-work.*' -delete"
+alias destroystack="find -name '.stack-work' -exec rm -rf {} +"
