247a7b3ad91247f1744cac3cfd3a8f1b0fef3623
Georgi Lyubenov <georgi.lyubenov@chaosgroup.com>
2019-02-25 21:10:51 +0200

    Stack: Add zsh autocomplete and some more aliases

--- zsh/custom/stack.zsh
+++ zsh/custom/stack.zsh
@@ -5,3 +5,11 @@ alias st="stack test"
 alias sghci="stack ghci"
 alias sghc="stack ghc"
 alias srunghc="stack runghc"
+
+alias swatch="stack build --file-watch --fast --test"
+alias spretty="stack ghci --ghci-options "-interactive-print=Text.Pretty.Simple.pPrint" --package pretty-simple"
+
+# autocomplete
+autoload -U +X compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+eval "$(stack --bash-completion-script stack)"
